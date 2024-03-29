f69049721fd4cfa3718b15cc6628f100bcb1baa7
Dmitry Ponyatov <dponyatov@gmail.com>
2024-03-30 00:57:03 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/meta.fs
+++ lib/meta.fs
@@ -6,3 +6,9 @@ let dotnet = "8.0"
 open System
 
 let project = Environment.CurrentDirectory.Split "/" |> Array.last
+
+
+let dirs = [ ".vscode"; "bin"; "doc"; "lib"; "inc"; "src"; "tmp" ]
+let dirs = [ meta ] @ List.map (fun s -> meta + "/" + s) dirs
+
+let mk dir = System.IO.Directory.CreateDirectory dir
