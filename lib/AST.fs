d3724d12dd9b62c5806a34f339a45d73d8bf134f
Dmitry Ponyatov <dponyatov@gmail.com>
2024-03-31 15:31:00 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/AST.fs
+++ lib/AST.fs
@@ -1,10 +1,20 @@
 module AST
 
-type Object(v: string) =
-    let value = v
+type Object =
+    val value: string
+    val nest: Object list
+    new(v: string) = { value = v; nest = [] }
 
 type Primitive(v: string) =
     inherit Object(v: string)
+
+type Num =
+    inherit Primitive
+    val value: float
+    new(f: float) = { value = f }
+    new(n: int) = { value = float (n) }
+    new(v: string) = { value = float (v) }
+
 
 type Container(v: string) =
     inherit Object(v: string)
---
+++ lib/primitive.fs
@@ -0,0 +1,9 @@
+module primitive
+
+type Primitive(v: string) =
+    inherit AST.Object(v: string)
+
+type Num =
+    inherit AST.Object
+    val value: float
+    new(v: string) = { value = float (v) }
